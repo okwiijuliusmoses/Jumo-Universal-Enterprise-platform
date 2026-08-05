3832b057151457c8e0122c993e62ae70107371ef
okwiijuliusmoses <okwiijuliusmoses@users.noreply.github.com>
2026-08-05 15:40:52 +0000

    fix(ueos): final resolve ErrorBoundary TS errors

--- src/App.tsx
+++ src/App.tsx
@@ -11,11 +11,8 @@ interface ErrorBoundaryState {
   error?: Error;
 }
 
-export class ErrorBoundary extends React.Component<ErrorBoundaryProps, ErrorBoundaryState> {
-  constructor(props: ErrorBoundaryProps) {
-    super(props);
-    this.state = { hasError: false };
-  }
+export class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
+  public state: ErrorBoundaryState = { hasError: false };
 
   public static getDerivedStateFromError(error: Error): ErrorBoundaryState {
     return { hasError: true, error };
